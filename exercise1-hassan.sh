c7951a491a826300da7c734df70963ba4dfb7503
akenawi <akenawi@emory.edu>
2020-09-23 22:51:18 +0000

    Committing exercise1-hassan.sh

---
+++ exercise1-hassan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Hassan Kenawi
+# The script takes one argument, a directory, and outputs the number of permission types in this directory
+
+ls -l | cut -d ' ' -f 1 | tail --lines=+2 | uniq | wc -l
+
